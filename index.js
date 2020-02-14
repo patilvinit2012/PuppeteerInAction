38eb6cf4ff2480a6f86e1449ef200e66e13014c2
patilvinit2012 <patilvinit2012@gmail.com>
2020-02-15 01:47:17 +0530

    mobile device scrapping

--- index.js
+++ index.js
@@ -2,30 +2,27 @@ const puppeteer = require('puppeteer');
 
 const config = {
 	getIncognito: true,//performance + isolation between pages
-	isHeadless: false// browser GUI
+	isHeadless: false,// browser GUI
+	emulate: {
+		doEmulate: true,
+		deviceName: 'iPhone 8'
+	}
 };
 
 (async () => {
-	let movieUrl = `https://www.imdb.com/title/tt1853728/?ref_=tt_sims_tti`;
+
+	console.log("hello");
+	let url = `https://www.imdb.com/title/tt1853728/?ref_=tt_sims_tti`;
 
 	let browser = await puppeteer.launch({headless: config.isHeadless});
 
 	let page = await getPage(browser);
 
-	await page.goto(movieUrl, { waitUntil: 'networkidle2'});
+	await emulateDevice(page);
 
+	await page.goto(url, { waitUntil: 'networkidle2'});
 
-	let data = await page.evaluate(() => {
-		let title = document.querySelector('div.title_wrapper > h1').innerText;
-		let ratingValue = document.querySelector('span[itemprop="ratingValue"]').innerText;
-		let ratingCount = document.querySelector('span[itemprop="ratingCount"]').innerText;
-
-		return {
-			title,
-			ratingValue,
-			ratingCount
-		}
-	})
+	let data = await evaluateScrapingLogic(page);
 
 	console.log(data);
 	await browser.close();
@@ -40,5 +37,41 @@ async function getPage(browser){
 	}else{
 
 		return await browser.newPage();
+	}
+};
+
+async function emulateDevice(page) {
+	if(config.emulate.doEmulate){
+		await page.emulate(puppeteer.devices[config.emulate.deviceName])
+	}
+};
+
+async function evaluateScrapingLogic(page) {
+
+	return page.evaluate(config.emulate.doEmulate ? getMobileDeviceSraping : getDesktopSraping );
+};
+
+function getDesktopSraping() {
+	let title = document.querySelector('div.title_wrapper > h1').innerText;
+	let ratingValue = document.querySelector('span[itemprop="ratingValue"]').innerText;
+	let ratingCount = document.querySelector('span[itemprop="ratingCount"]').innerText;
+
+	return {
+		title,
+		ratingValue,
+		ratingCount
+	}
+};
+
+function getMobileDeviceSraping() {
+
+	let title = document.querySelector('.media-body > h1').innerText
+	let ratingValue = document.querySelector('#ratings-bar').innerText.split('\n')[0];
+	let ratingCount = document.querySelector('#ratings-bar').innerText.split('\n')[1].split(' ')[0];
+
+	return {
+		title,
+		ratingValue,
+		ratingCount
 	}
 };
